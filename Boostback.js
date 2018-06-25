ee681a375aae5bc1955ea38956bc313fef558aa5
Dennis O'Flaherty <dennis.oflaherty@gmail.com>
2018-06-24 23:08:19 -0400

    Fix mobile header

--- Boostback.js
+++ Boostback.js
@@ -110,7 +110,7 @@ class Navigation extends React.Component {
 			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span className="navbar-toggler-icon"></span>
 			</button>
-			<div className="collapse navbar-collapse">
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul className="navbar-nav ml-auto">
 					<li className="nav-item">
 						<a className="nav-link">Select a vehicle and mission:</a>
